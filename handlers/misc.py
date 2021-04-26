cb9daa65461c4fae51cb7685da8130db506a5369
sangramghangale <81154807+sangramghangale@users.noreply.github.com>
2021-04-26 11:53:05 +0530

    Update misc.py

--- handlers/misc.py
+++ handlers/misc.py
@@ -1,17 +1,23 @@
-HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
-
-**/help** __Show This Message.__
-**/skip** __Skip The Current Playing Music.__
-**/play** __youtube/saavn/deezer Song_Name__
-**/telegram** __Play From Telegram Audio.__
-**/joinvc** __Join Voice Chat.__
-**/rejoinvc** __Rejoin Voice Chat.__
-**/leavevc** __Leave Voice Chat.__
-**/volume [1-200]** __Adjust Volume.__
-**/pause** __Pause Music.__
-**/resume** __Resume Music.__
-**/update** __Update & Restart.__"""
-
+HELP_TEXT = """<b>Hi {message.from_user.first_name}!
+\n/play <song name> - play song you requested
+/dplay <song name> - play song you requested via deezer
+/splay <song name> - play song you requested via jio saavn
+/playlist - Show now playing list
+/current - Show now playing
+/song <song name> - download songs you want quickly
+/search <query> - search videos on youtube with details
+/deezer <song name> - download songs you want quickly via deezer
+/saavn <song name> - download songs you want quickly via saavn
+/video <song name> - download videos you want quickly
+\n*Admins only*
+/player - open music player settings panel
+/pause - pause song play
+/resume - resume song play
+/skip - play next song
+/end - stop music play
+/userbotjoin - invite assistant to your chat
+/admincache - Refresh admin list
+ </b>""",
 
 REPO_TEXT = "[Github](https://github.com)" \
             + " | [Group](t.me/YouTubeMusicPlayerRoBot)"
