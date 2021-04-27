801ebe3ebd34eaba7b6d3c3e8a49b65ff0b3f581
prince301102 <79126372+prince301102@users.noreply.github.com>
2021-04-27 23:26:16 +0530

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -20,25 +20,25 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "YouTubeMusicPlayerRoBot"
+        user.first_name =  "prince_vcmusic01_plugin"
 
     try:
         await USER.join_chat(invitelink)
         await USER.send_message(message.chat.id,"I joined here as you requested")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>@YouTubeMusicPlayerRoBot already in your chat</b>",
+            "<b>Your user bot already in your chat</b>",
         )
         pass
     except Exception as e:
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @YouTubeMusicPlayerRoBot to your Group and try again</b>",
+            "\n\nOr manually add your userbot your Group and try again</b>",
         )
         return
     await message.reply_text(
-            "<b>@YouTubeMusicPlayerRoBot userbot joined your chat</b>",
+            "<b>Your userbot joined your chat</b>",
         )
     
 @USER.on_message(filters.group & filters.command(["userbotleave"]))
