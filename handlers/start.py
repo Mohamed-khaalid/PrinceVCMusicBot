62444d5a9f6b10f861bf48d864a3c0b72a620ca0
prince301102 <79126372+prince301102@users.noreply.github.com>
2021-05-03 09:04:18 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -80,47 +80,6 @@ async def start(client: Client, message: Message):
     & filters.group
     & ~ filters.edited
 )
-async def start(client: Client, message: Message):
-    await message.reply_text(
-        "⚜️HELP SECTION⚜️
-
-⚜️Users Commands⚜️
-/play <song name> - play song you requested
-/dplay <song name> - play song you requested via deezer
-/splay <song name> - play song you requested via jio saavn
-/playlist - Show now playing list
-/current - Show now playing
-/song <song name> - download songs you want quickly
-/search <query> - search videos on youtube with details
-/deezer <song name> - download songs you want quickly via deezer
-/saavn <song name> - download songs you want quickly via saavn
-/video <song name> - download videos you want quickly
-
-⚜️Admins only⚜️
-/player - open Music player settings panel
-/pause - pause song play
-/resume - resume song play
-/skip - play next song
-/end - stop music play
-/userbotjoin - invite assistant to your chat
-/admincache - Refresh admin list
- </b>""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🔊 Channel", url="https://t.me/prince_vcmusic_bot_channel"
-                    )
-                ]
-            ]
-        )
-    )    
-"
-@Client.on_message(
-    filters.command("help")
-    & filters.private
-    & ~ filters.edited
-)
 async def help(client: Client, message: Message):
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name}!
